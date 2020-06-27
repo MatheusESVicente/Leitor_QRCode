35246416c60674753dd4a61543826deb51c7d50c
MatheusESVicente <50384915+MatheusESVicente@users.noreply.github.com>
2020-06-26 23:09:54 -0300

    Leitor_QRCode

---
+++ src/app/model/Historico.ts
@@ -0,0 +1,4 @@
+export class Historico {
+    leitura: string;
+    dataHora: string;
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { Historico } from '../model/Historico';
 
 
 
@@ -10,11 +11,18 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 })
 export class Tab2Page {
 
-  public Hora:string = "14:15";
-  public Data:string = "22/06/2020";
-
+  private historicos: Historico[] = [
+    { dataHora: "21/06/2020 18:25", leitura: "http://www.ronanzenatti.com" },
+    { dataHora: "22/06/2020 18:30", leitura: "https://github.com/MatheusESVicente" },
+    { dataHora: "23/06/2020 18:35", leitura: "https://ionicframework.com/" },
+    { dataHora: "24/06/2020 18:40", leitura: "http://www.etecbarrabonita.com.br/" },
+    { dataHora: "25/06/2020 18:45", leitura: "Primeiro teste" },
+  ];
+ 
 
   constructor(private screenOrientation: ScreenOrientation) {}
+
+  
 
   
 }
