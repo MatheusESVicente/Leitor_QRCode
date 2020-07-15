21c0755a065a54cbf32bf6d707d7f497fd7f9908
MatheusESVicente <50384915+MatheusESVicente@users.noreply.github.com>
2020-07-15 17:10:59 -0300

    Leitor_QRCode

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,9 @@ import { Dialogs } from '@ionic-native/dialogs/ngx';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { Platform } from '@ionic/angular';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { HistoricoService } from '../services/historico.service';
+import { Historico } from '../model/Historico';
+import { async } from '@angular/core/testing';
 
 
 @Component({
@@ -17,7 +20,12 @@ export class Tab1Page {
   public scanner: any;
   public link: false;
 
-  constructor(private qrScanner: QRScanner, private dialogs: Dialogs, private platform: Platform, private screenOrientation: ScreenOrientation) {
+  constructor(private qrScanner: QRScanner,
+     private dialogs: Dialogs, 
+     private platform: Platform, 
+     private screenOrientation: ScreenOrientation,
+     private historicoService:HistoricoService
+     ) {
     this.platform.backButton.subscribeWithPriority(0, ()=>{
       this.corpoPagina.style.opacity = '1';
       this.img.style.opacity = '1';
@@ -28,7 +36,7 @@ export class Tab1Page {
     })
   }
 
-  public lerQRCode(){
+  public async lerQRCode(){
     // Optionally request the permission early
 this.qrScanner.prepare()
 .then((status: QRScannerStatus) => {
@@ -38,12 +46,25 @@ this.qrScanner.prepare()
      this.img = document.getElementById('logo') as HTMLElement;
      this.corpoPagina.style.opacity = "0";
      this.img.style.opacity = "0";
-     this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+
+     this.scanner = this.qrScanner.scan().subscribe(async(text: string) => {
+
       this.valor_leitura= (text['result']) ? text['result'] : text;
       this.corpoPagina.style.opacity = "1";
       this.img.style.opacity = "1";
       this.qrScanner.hide(); // hide camera preview
       this.scanner.unsubscribe(); // stop scanning
+
+      const historico = new Historico();
+      historico.leitura = this.valor_leitura;
+      historico.dataHora = new Date();
+
+      await this.historicoService.create(historico).then(resposta =>{
+        console.log(resposta);
+      }).catch(error =>{
+        this.presentAlert('ERRO',"Erro ao salvar no Firebase.");
+        console.log('ERRO: ', error);
+      });
     });
 
 
@@ -55,6 +76,9 @@ this.qrScanner.prepare()
 })
 .catch((e: any) => console.log('Error is', e));
 
+  }
+  presentAlert(arg0: string, arg1: string) {
+    throw new Error("Method not implemented.");
   }
 }
 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { Historico } from '../model/Historico';
+import { HistoricoService } from '../services/historico.service';
+import { Subscriber, from } from 'rxjs';
+
+import localePtBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 
 
@@ -11,18 +16,36 @@ import { Historico } from '../model/Historico';
 })
 export class Tab2Page {
 
-  private historicos: Historico[] = [
-    { dataHora: "21/06/2020 18:25", leitura: "http://www.ronanzenatti.com" },
-    { dataHora: "22/06/2020 18:30", leitura: "https://github.com/MatheusESVicente" },
-    { dataHora: "23/06/2020 18:35", leitura: "https://ionicframework.com/" },
-    { dataHora: "24/06/2020 18:40", leitura: "http://www.etecbarrabonita.com.br/" },
-    { dataHora: "25/06/2020 18:45", leitura: "Primeiro teste" },
-  ];
+  private historicos: Historico[] = [];
  
 
-  constructor(private screenOrientation: ScreenOrientation) {}
+  constructor(private screenOrientation: ScreenOrientation,
+    private historicoService: HistoricoService,
+    ) {
+      registerLocaleData(localePtBr);
+    }
 
-  
+  public buscarHistoricos(){
+    this.historicos = [];
 
-  
+    this.historicoService.getAll().subscribe(dados =>{
+      this.historicos = dados.map(register =>{
+        return {
+          $key: register.payload.doc.id,
+          leitura: register.payload.doc.data()['leitura'],
+          dataHora: new Date(register.payload.doc.data()['dataHora']['secunds'] * 1000)
+         } as Historico;
+      });
+    });
+  }
+
+  async ionViewWillEnter(){
+    await this.buscarHistoricos();
+  }
+
+  public deletar(key: string){
+    this.historicoService.delete(key);
+    this.buscarHistoricos();
+  }
+
 }
